8883244e7585d9b7411aa26e562a1051cb018ffb
jiyakosambiya75-design <jiyakosambiya75@gmail.com>
2026-06-25 18:07:51 +0530

    Add files via upload

---
+++ jiya25pdf.py
@@ -0,0 +1,26 @@
+#1. Store and print name, age, and city
+
+print("Hello! my name is jiya")
+print("Im 18 years old")
+print("I live in surat gujarat")
+#2. Take user input and display formatted output
+
+name=input("What is your name: ")
+print(name)
+#3. Swap two variables
+
+a=int(input("Enter your first num: "))
+b=int(input("Enter your Second num: "))
+a,b=b,a
+print("a=",a)
+print("b=",b)
+#4. Find square and cube of a number
+
+num=int(input("Enter a number: "))
+print("square=",num**2)
+print("square=",num**3)
+#5. Convert temperature (Celsius → Fahrenheit)
+
+celsius=float(input("Enter temperature in celsius: "))
+fahrenheit=(celsius*9/5)+32
+print("Temprature in fahrenheit=",fahrenheit)
