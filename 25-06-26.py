ef74b8c681aef659f30471763d7d4f7040351104
jiyakosambiya75-design <jiyakosambiya75@gmail.com>
2026-06-25 16:43:32 +0530

    Add files via upload

---
+++ 25-06-26.py
@@ -0,0 +1,113 @@
+Python 3.14.6 (tags/v3.14.6:c63aec6, Jun 10 2026, 10:26:10) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+
+============= RESTART: C:/Users/jiya kosambiya/Documents/jiya python/jiya.python/24-06-26/24-06-26.py ============
+addition 30
+substraction -10
+multiplication 200
+division 0.5
+modulus 10
+Exponention 100000000000000000000
+floor division 0
+
+============= RESTART: C:/Users/jiya kosambiya/Documents/jiya python/jiya.python/24-06-26/24-06-26.py ============
+addition 30
+substraction -10
+multiplication 200
+division 0.5
+modulus 10
+Exponention 100000000000000000000
+floor division 0
+8
+3
+
+============= RESTART: C:/Users/jiya kosambiya/Documents/jiya python/jiya.python/24-06-26/24-06-26.py ============
+8
+3
+5
+3
+
+============= RESTART: C:/Users/jiya kosambiya/Documents/jiya python/jiya.python/24-06-26/24-06-26.py ============
+8
+3
+5
+3
+1.6666666666666667
+3
+1.6666666666666667
+3
+4.629629629629631
+3
+1.0
+3
+
+
+============= RESTART: C:/Users/jiya kosambiya/Documents/jiya python/jiya.python/24-06-26/24-06-26.py ============
+False
+
+============= RESTART: C:/Users/jiya kosambiya/Documents/jiya python/jiya.python/24-06-26/24-06-26.py ============
+True
+False
+False
+True
+False
+True
+
+============= RESTART: C:/Users/jiya kosambiya/Documents/jiya python/jiya.python/24-06-26/24-06-26.py ============
+False
+True
+Traceback (most recent call last):
+  File "C:/Users/jiya kosambiya/Documents/jiya python/jiya.python/24-06-26/24-06-26.py", line 51, in <module>
+    print(x<y)
+TypeError: '<' not supported between instances of 'int' and 'str'
+
+============= RESTART: C:/Users/jiya kosambiya/Documents/jiya python/jiya.python/24-06-26/24-06-26.py ============
+True
+False
+False
+True
+False
+True
+10
+
+============= RESTART: C:/Users/jiya kosambiya/Documents/jiya python/jiya.python/24-06-26/24-06-26.py ============
+Traceback (most recent call last):
+  File "C:/Users/jiya kosambiya/Documents/jiya python/jiya.python/24-06-26/24-06-26.py", line 60, in <module>
+    x=true
+NameError: name 'true' is not defined. Did you mean: 'True'?
+
+============= RESTART: C:/Users/jiya kosambiya/Documents/jiya python/jiya.python/24-06-26/24-06-26.py ============
+Traceback (most recent call last):
+  File "C:/Users/jiya kosambiya/Documents/jiya python/jiya.python/24-06-26/24-06-26.py", line 60, in <module>
+    x = true
+NameError: name 'true' is not defined. Did you mean: 'True'?
+>>> 
+============= RESTART: C:/Users/jiya kosambiya/Documents/jiya python/jiya.python/24-06-26/24-06-26.py ============
+False
+False
+True
+False
+False
+True
+>>> 
+============= RESTART: C:/Users/jiya kosambiya/Documents/jiya python/jiya.python/24-06-26/24-06-26.py ============
+<class 'str'>
+<class 'int'>
+>>> 
+============= RESTART: C:/Users/jiya kosambiya/Documents/jiya python/jiya.python/24-06-26/24-06-26.py ============
+<class 'str'>
+<class 'int'>
+130
+>>> 
+============= RESTART: C:/Users/jiya kosambiya/Documents/jiya python/jiya.python/24-06-26/24-06-26.py ============
+10 20 30
+>>> 
+============= RESTART: C:/Users/jiya kosambiya/Documents/jiya python/jiya.python/24-06-26/24-06-26.py ============
+10 20 30
+140709071734488
+140709071734488
+>>> 
+============= RESTART: C:/Users/jiya kosambiya/Documents/jiya python/jiya.python/24-06-26/24-06-26.py ============
+10 20 30
+140709071734488
+140709071734488
