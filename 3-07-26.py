2a9f52db3a8fdb461137da9274d0b24aea6119a6
jiyakosambiya75-design <jiyakosambiya75@gmail.com>
2026-07-09 17:54:47 +0530

    Add files via upload

---
+++ 3-07-26.py
@@ -0,0 +1,15 @@
+#python collection
+print("===== python collection =====")
+
+#list
+list1=[10,20,30,40]
+
+print(type(list1))
+print(list1[0])
+print(list1[1])
+print(list1[2])
+print(list1[3])
+
+for i in list1:
+    print(i)
+
